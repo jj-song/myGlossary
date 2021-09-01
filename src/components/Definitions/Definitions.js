4082c12f52018a340c5fbb2698a5d91731ad2954
Song <jsong350@gatech.edu>
2021-08-31 23:15:54 -0400

    added temporary definitions

--- src/components/Definitions/Definitions.js
+++ src/components/Definitions/Definitions.js
@@ -25,12 +25,12 @@ const Definitions = ({word, category, meanings, LightMode}) => {
                         mean.meanings.map((item) => (
                             item.definitions.map((def) => (
                                 <div className="singleMean" style={{backgroundColor:LightMode?"#3b5360":"white", color: LightMode? "white": "black"}}>
-                                    <b> Definition: Travel Issuance and Overseas Citizen Assistance.</b>
+                                    <b> Definition:</b> Travel Issuance and Overseas Citizen Assistance. comprised of the following programs: 1. Passport Support Services (PSS), 2. American Citizen Services (ACS), 3. Non-Citizen Services (NCS), and 4. Consular Lookout and Support Systems (CLASS).
                                     <hr style={{backgroundColor:"black", width: "100%"}} />
                                     {
                                         meanings[0].phonetic && (
                                             <span>
-                                                <b>Pronunciation: TIE-OH-KA </b>
+                                                <b>Pronunciation:</b> TIE-OH-KA 
                                             </span>
                                         )
                                     }
@@ -46,12 +46,12 @@ const Definitions = ({word, category, meanings, LightMode}) => {
                         mean.meanings.map((item) => (
                             item.definitions.map((def) => (
                                 <div className="singleMean" style={{backgroundColor:LightMode?"#3b5360":"white", color: LightMode? "white": "black"}}>
-                                    <b> Definition: American Citizen Services.</b>
+                                    <b> Definition:</b> American Citizen Services. An umbrella of applications which includes, but is not limited to: passport services, notary services, and Consular Reports of Births Abroad. The U.S. Consulate General also assists in emergencies, including deaths, arrests and crisis situations.
                                     <hr style={{backgroundColor:"black", width: "100%"}} />
                                     {
                                         meanings[0].phonetic && (
                                             <span>
-                                                <b>Pronunciation: AE-SEE-ES </b>
+                                                <b>Pronunciation:</b> AE-SEE-ES 
                                             </span>
                                         )
                                     }
@@ -67,12 +67,12 @@ const Definitions = ({word, category, meanings, LightMode}) => {
                         mean.meanings.map((item) => (
                             item.definitions.map((def) => (
                                 <div className="singleMean" style={{backgroundColor:LightMode?"#3b5360":"white", color: LightMode? "white": "black"}}>
-                                    <b> Definition: International Parental Child Support.</b>
+                                    <b> Definition:</b> International Parental Child Support. Application which facilitates the U.S. Department of State's assistance in addressing the removal or retention of a child outside their country of habitual residence in breach of another parent or guardian’s custody rights.
                                     <hr style={{backgroundColor:"black", width: "100%"}} />
                                     {
-                                        meanings[0].phonetic && (
+                                        (
                                             <span>
-                                                <b>Pronunciation: IHP-KA </b>
+                                                <b>Pronunciation:</b> IHP-KA 
                                             </span>
                                         )
                                     }
